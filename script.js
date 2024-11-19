9deeca5bee7257628e7a8dcec2f788b0d1146228
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2024-11-19 11:13:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("keydown", (event) =>{
+  const key = event.key.toUpperCase(); // CAPTURA TECLA PRESSIONADA
+  const teclas = document.querySelectorAll(".tecla");
+
+  teclas.forEach((tecla) =>{
+    if (tecla.classList.contains("space") && key === " "){
+      tecla.classList.add("ativa");// ADICIONA ANIMAÇÃO
+    } else if (tecla.textContent.toUpperCase() === key){
+      tecla.classList.add("ativa");// ADICIONA ANIMAÇÃO
+    }
+  });
+});
+
+document.addEventListener("keyup", (event) =>{
+  const key = event.key.toUpperCase(); // CAPTURA TECLA PRESSIONADA
+  const teclas = document.querySelectorAll(".tecla");
+
+  teclas.forEach((tecla) =>{
+    if (tecla.classList.contains("space") && key === " "){
+      tecla.classList.remove("ativa"); // REMOVE ANIMAÇÃO
+    } else if (tecla.textContent.toUpperCase() === key){
+      tecla.classList.remove("ativa"); // REMOVE ANIMAÇÃO
+    }
+  });
+});
