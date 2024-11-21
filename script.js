00587bc7c76bed06e1a5b6d6ea93934bcfe14b39
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2024-11-21 10:22:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// ELEMENTOS DO TECLADO
+const linhasTeclas = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["a", "s", "d", "f", "g", "h", "j", "k", "l", "ç"],
+  ["z", "x", "c", "v", "b", "n", "m"],
+  ["Espace"]
+];
+// REFERÊNCIA AO ELEMENTO TECLADO
+const teclado = document.querySelector(".teclado");
+// FUNÇÃO PARA CRIAR O TECLADO
+linhasTeclas.forEach((linha) =>{
+  const divLinha = document.createElement("div");
+  divLinha.classList.add("linha");
+
+  linha.forEach((tecla) =>{
+    const divTecla = document.createElement("div");
+    divTecla.classList.add("tecla");
+    divTecla.textContent = tecla;
+
+    if (tecla.toLowerCase() === "espace"){
+      divTecla.classList.add("space");
+    }
+
+    divLinha.appendChild(divTecla);
+  });
+
+  teclado.appendChild(divLinha);
+});
+// ADICIONA O LINK PARA O GITHUB
+const link = document.createElement("a");
+link.href = "https://github.com/Lucasmassaroto1";
+link.target = "_blank";
+link.textContent = "@Lucasmassaroto1";
+teclado.appendChild(link);
+// CRIA A ANIMAÇÃO
 document.addEventListener("keydown", (event) =>{
   const key = event.key.toUpperCase(); // CAPTURA TECLA PRESSIONADA
   const teclas = document.querySelectorAll(".tecla");
@@ -10,6 +45,7 @@ document.addEventListener("keydown", (event) =>{
     }
   });
 });
+
 document.addEventListener("keyup", (event) =>{
   const key = event.key.toUpperCase(); // CAPTURA TECLA PRESSIONADA
   const teclas = document.querySelectorAll(".tecla");
