8eb5b461d5d58f2f5746821303026d28f82f9600
Lucas Massaroto <102060111+Lucasmassaroto1@users.noreply.github.com>
2024-11-21 10:02:33 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ document.addEventListener("keydown", (event) =>{
     }
   });
 });
-
 document.addEventListener("keyup", (event) =>{
   const key = event.key.toUpperCase(); // CAPTURA TECLA PRESSIONADA
   const teclas = document.querySelectorAll(".tecla");
@@ -21,16 +20,5 @@ document.addEventListener("keyup", (event) =>{
     } else if (tecla.textContent.toUpperCase() === key){
       tecla.classList.remove("ativa"); // REMOVE ANIMAÇÃO
     }
-  });
-});
-
-// ATIVA ANIMAÇÕES AO CLICAR NO TECLADO VIRTUAL
-document.querySelectorAll(".tecla").forEach((tecla) => {
-  tecla.addEventListener("touchstart", () => {
-    tecla.classList.add("ativa"); // ADICIONA ANIMAÇÃO
-  });
-
-  tecla.addEventListener("touchend", () => {
-    tecla.classList.remove("ativa"); // REMOVE ANIMAÇÃO
   });
 });
